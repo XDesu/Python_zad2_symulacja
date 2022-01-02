826fe776b49869df8d00c3673455da4240214e75
Adam Kapuściński <ziemniaczek320@gmail.com>
2022-01-02 21:15:06 +0100

    prawie na 4

--- main.py
+++ main.py
@@ -3,18 +3,6 @@ import argparse
 
 from entity.simulation import Simulation
 
-# problemy do rozwiązania:
-# - jak radzimy sobie z ustawieniami domyślnymi?
-#   - zmienne w main (brzydkie i chyba trochę nie eleganckie)
-#   - domyślne wartości kontruktorów (niebezpieczeństwo, że ktoś może podać None/null)
-#   - jakiś plik defaults.py z wartościami domyślnymi (tylko wtedy gdzie go umieścić? Problem struktury folderów)
-
-# czy tworzymy klasę "łąki", która będzie się zajmować iteracją między etapami i tylko udostępniać
-# obiekty / informacje już sformatowane, czy przyjmujemy konwencje, że udostępniamy klasy
-# i nie obchodzi nas to czy będą dobrze używane
-
-# doci piszemy po polsku czy angielsku?
-
 
 def load_config(config_file_path: str = ""):
     config = configparser.ConfigParser()
